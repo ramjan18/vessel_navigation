8370bf42ea563ac9cec396067d5468ee513a2889
Ramjan <ramjannadaf487@gmail.com>
2024-08-11 17:12:16 +0530

    map added

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -15,6 +15,10 @@ export default function MapComponent({ startlat, startlng, endlat, endlng, speed
  
     <div>
        <MapContainer center={[startlat, startlng]} zoom={11} style={{ height: '100vh', width: '100%' }}></MapContainer>
+       <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
     </div>
   )
 }
